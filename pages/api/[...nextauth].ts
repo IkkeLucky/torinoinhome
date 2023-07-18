5e33381a2fad860e20ec1ff7634cdd89b3420fca
ikkelucky <nachomarchesini@gmail.com>
2023-07-18 17:53:17 +0200

    credentials

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -60,7 +60,8 @@ export const authOptions: AuthOptions = {
     },
     debug: process.env.NODE_ENV === 'development',
     session: {
-        strategy: "jwt"
+        strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
 };
+
